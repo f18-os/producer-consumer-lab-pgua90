1b96d76aa39c0c5632838ad1f7986d28480336d6
student <[pgua90@yahoo.com]>
2018-11-21 21:49:25 -0700

    update

--- ExtractAndDisplay.py
+++ ExtractAndDisplay.py
@@ -1,99 +1,100 @@
 #!/usr/bin/env python3
 #! /usr/bin/pip
+import cv2
 import os
 import threading
-import cv2
 import base64
 import queue
 
-frame_1 = []
-frame_2 = []
-semaphore_1 = threading.Semaphore(10)
-semaphore_2 = threading.Semaphore(10)
-semaphore_3 = threading.Semaphore(10)
-semaphore_4 = threading.Semaphore(10)
-lock_1 = threading.Lock()
-lock_2 = threading.Lock()
+frame1 = []
+frame2 = []
+lock1 = threading.Lock()
+semaphore1 = threading.Semaphore(10)
+semaphore2 = threading.Semaphore(10)
+lock2 = threading.Lock()
+semaphore3 = threading.Semaphore(10)
+semaphore4 = threading.Semaphore(10)
 
 class extractFrames(threading.Thread):
     def __init__(self):
         super(extractFrames, self).__init__()
-
-    def extracting(self):
-        filename = 'clip.mp4'
-        vidcap = cv2.VideoCapture(fileName)    
-        success = vidcap.read()
-        image = vidcap.read()
+    def run(self):
+        clip = 'clip.mp4'
+        vidcap = cv2.VideoCapture(clip)
+        success, image = vidcap.read()
         count = 0
         print("Reading frame {} {} ".format(count, success))
         count+=1
         while success:
-            semaphore_1.acquire()
-            lock_1.acquire()
-            frame_1.append(image)
+            semaphore1.acquire()
+            lock1.acquire()
+            frame1.append(image)
             success,image = vidcap.read()
             print('Reading frame {} {}'.format(count, success))
             count += 1
-            lock_1.release()
-            semaphore_1.release()
+            lock1.release()
+            semaphore1.release()
         for x in range(10):
-            semaphore_1.release()
+            semaphore1.release()
         print("Frame extraction complete")
 
+#Frames are converted and sent to its queue.
 class convertFrames(threading.Thread):
     def __init__(self):
         super(convertFrames, self).__init__()
-    def converting(self):
+
+    def run(self):
         count2 = 0
         while True:
-            semaphore_2.acquire()
-            semaphore_3.acquire()
-            lock_1.acquire()
-            lock_2.acquire()
-            if(frame_1):
-                freeFrame = frame_1.pop()
+            semaphore2.acquire()
+            semaphore3.acquire()
+            lock1.acquire()
+            lock2.acquire()
+            if(frame1):
+                freeFrame = frame1.pop()
             else:
                 break
-            grayFrame = cv2.cvtColor(freeFrame, cv2.COLOR_BGR2GRAY)
-            frame_2.append(grayFrame)
-            print("Converting frame {}".format(count))
+            grayFrame = cv2.cvtColor(freeFrame, cv2.COLOR_BGR2GRAY) #Frame converts to greyscale
+            frame2.append(grayFrame)
+            print("Converting frame {}".format(count2))
             count2 += 1
-            lock_2.release()
-            lock_1.release()
-            semaphore_4.release()
-            semaphore_1.release()
-        lock_2.release()
-        lock_1.release()
-        semaphore_1.release()
+            lock2.release()
+            lock1.release()
+            semaphore4.release()
+            semaphore1.release()
+        lock2.release()
+        lock1.release()
+        semaphore1.release()
         for x in range(10):
-            semaphore_4.release()
+            semaphore4.release()
         
 class displayFrames(threading.Thread):
     def __init__(self):
         super(displayFrames, self).__init__()
-    def displaying(self):
+    def run(self):
         while True:
-            semaphore_4.acquire()
-            lock_2.acquire()
-            if(frame_2):
-                freeFrame2 = frame_2.pop()
-                cv2.imshow("Video",freeFrame2)
+            semaphore4.acquire()
+            lock2.acquire()
+            if(frame2):
+                freeFrame2 = frame2.pop()
+                cv2.imshow("Video", freeFrame2)
                 if cv2.waitKey(24) and 0xFF == ord("q"):
                     break
             else:
-                print("Finished displaying all frames")
                 break
-            lock_2.release()
-            semaphore_4.release()
+            lock2.release()
+            semaphore3.release()
 
 for x in range(10):
-    semaphore_2.acquire()
-    semaphore_4.acquire()
+    semaphore2.acquire()
+    semaphore4.acquire()
 
-toDisplay = displayFrames()
-toConvert = convertFrames()
-toExtract = extractFrames()
+dis = displayFrames()
+con = convertFrames()
+ext = extractFrames()
 
-toDisplay.start()
-toConvert.start()
-toExtract.start()
+ext.start()
+
+con.start()
+
+dis.start()
